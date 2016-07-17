7795c68704e3d9efa669b7b40e5ee6cf573a3de8
Gaurav Kaushik <gaurav@Gaurav.local>
2016-07-17 17:19:34 -0400

    Added ability to keep additional info; cleaning for workflows; tested on nested

--- purify.py
+++ purify.py
@@ -9,40 +9,61 @@ def purify(input_dict):
     :param file: input JSON file
     This will do some checks to purify a CWL JSON file pulled from the platform.
     Now you can easily share externally (e.g. on GitHub) or add to other projects without links.
+    Note the following:
+    - custom fields inside ports are not scrubbed so IO behavior is maintained (may be tool-critical)
+    - for workflows, "sbg:id" cannot be deleted or be empty, so we set it and "id" to "-"
     """
-    tool = input_dict
-    for k in tool.keys():
-        if k.startswith("sbg:"):
-            tool.pop(k)
-    tool["id"] = ""
-    return tool
+    app = input_dict
 
-def object2json(input_tool):
+    # 1. Empty the ids
+    app["id"], app["sbg:id"] = "-", "-"
+
+    # 2. Remove any sbg-specific fields not in the whitelist. This is enough for tools.
+    field_whitelist = ["sbg:categories", "sbg:id", "sbg:toolAuthor", "sbg:toolkit"]
+    for k in app.keys():
+        if k.startswith("sbg:") and k not in field_whitelist:
+            app.pop(k)
+
+    # 3. Workflows are more complicated. They have some extensions with are required for proper importing and saving.
+    #       In the case of workflow:
+    #           1. Check that it's a workflow (should have the "steps" key)
+    #           2. Remove any "sbg:" fields from "run" subfield
+    if "steps" in app:
+        for idx, values in enumerate(app["steps"]):
+            for step_key in values.keys():
+                if step_key.startswith("run"):
+                    for run_key in values["run"].keys():
+                        if run_key.startswith("sbg:"):
+                            app["steps"][idx]["run"].pop(str(run_key))
+                        if run_key.startswith("id"):
+                            app["steps"][idx]["run"]["id"] = ""
+    return app
+
+def object2json(input_app):
     """ methods to clean up the object when converting to JSON """
-    data = json.dumps(input_tool, default=lambda o: clean_null_values(o.__dict__),
+    app = json.dumps(input_app, default=lambda o: clean_null_values(o.__dict__),
                             sort_keys=True, skipkeys=True, indent=2)
-    data = data.replace("class_", "class")
-    return data
-
+    app = app.replace("class_", "class")
+    return app
 
 if __name__ == "__main__":
     # parse those args
     parser = argparse.ArgumentParser()
     parser.add_argument("-j", "--json", default=None, help="Specify path to CWL file")
-    parser.add_argument("-o", "--output", default=None, help="Redirect to an output file (ext: .cwl)") # choose whether to save
+    parser.add_argument("-o", "--output", default=None, help="Redirect to an output file (ext: .cwl)")
     args = parser.parse_args()
     args_json = args.json
 
-    # purify the tool
-    tool = CwlReader(in_json=args_json)
-    tool_dict = tool.Tool.__dict__
-    purified_tool = purify(tool_dict)
+    # purify the CWL application
+    input_app = CwlReader(in_json=args_json)
+    app_dict = input_app.Tool.__dict__
+    purified_app = purify(app_dict)
 
     # save if desired or print to console
     if args.output:
         args_file = args.output + ".cwl"
         with open(args_file, "w") as output:
-            print(object2json(purified_tool), file=output)
+            print(object2json(purified_app), file=output)
         print("Purified file saved at: " + args_file)
     else:
-        print(object2json(purified_tool))
+        print(object2json(purified_app))
